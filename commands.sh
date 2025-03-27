a285c12331601134bfc43fccfcd01d50fd97cc62
m4nihere <manishkumarmourya@proton.me>
2025-03-27 12:02:12 +0000

    v3

---
+++ commands.sh
@@ -0,0 +1,9 @@
+  sudo cp /etc/letsencrypt/live/es.codecrumbs.cloud/fullchain.pem ~/elastic-certs/es01.crt
+  sudo cp /etc/letsencrypt/live/es.codecrumbs.cloud/privkey.pem ~/elastic-certs/es01.key
+  sudo cp /etc/letsencrypt/live/es.codecrumbs.cloud/fullchain.pem ~/elastic-certs/kibana.crt
+  sudo cp /etc/letsencrypt/live/es.codecrumbs.cloud/privkey.pem ~/elastic-certs/kibana.key
+  sudo cp /etc/letsencrypt/live/es.codecrumbs.cloud/fullchain.pem ~/elastic-certs/fleet-server.crt
+  sudo cp /etc/letsencrypt/live/es.codecrumbs.cloud/privkey.pem ~/elastic-certs/fleet-server.key 
+
+
+  
